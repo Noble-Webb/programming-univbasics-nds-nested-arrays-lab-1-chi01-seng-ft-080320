c19ab1d92df4df276dbd1ebc88dca9c362e579ed
Justin Webb <webb.n.justin@gmail.com>
2020-07-19 02:48:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,7 +37,7 @@ end
 
 def matrix_lookup(matrix, row, column)
   produce_storage = [
-    ORGANIC_PRODUCE
+    CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE
     ]
   produce_storage[1][1]
   # Given any matrix (array of arrays), a row index and a column index, 
