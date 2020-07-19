c00c716b95d9bb1ab1b89216d505330e54f3af6c
Justin Webb <webb.n.justin@gmail.com>
2020-07-19 02:46:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,13 +36,9 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus"
-]
+  produce_storage = [
+    CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE
+    ]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
